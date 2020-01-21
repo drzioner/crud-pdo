bd88b1c7efad48ee87f1e908483f9b35cbe0df3c
Deiner Jose Vega Hernandez <drzioner@gmail.com>
2020-01-20 23:47:33 -0500

    Iniciando la creacion de la estructura de la clase Crud

---
+++ src/Crud.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace drzioner\CrudPdo;
+
+/**
+ *
+ */
+class Crud
+{
+
+ public function __construct()
+ {
+  // code...
+ }
+
+ public function create()
+ {
+  // code...
+ }
+
+ public function read()
+ {
+  // code...
+ }
+
+ public function update()
+ {
+  // code...
+ }
+
+ public function delete()
+ {
+  // code...
+ }
+}
